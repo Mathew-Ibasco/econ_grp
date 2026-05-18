e796f40469222edee2f6cc85bbd4a1cb9d0f3205
Mathew-Ibasco <mathew.ibasco@uap.asia>
2026-05-18 10:23:26 +0800

    login and database (please read models.py)

--- econ/urls.py
+++ econ/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path, include
+from django.urls import path
 from econ import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
-from django.shortcuts import render
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("login_process/", views.login_process, name="login_process"),
+    path("logout_process/", views.logout_process, name="logout_process"),
+    path("admin_dashboard/", views.admin_dashboard, name="admin_dashboard"),
+    path("registration/", views.registration, name="registration"),
+    path("registration_process/", views.registration_process, name="registration_process"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ econdb.sql
@@ -0,0 +1,2 @@
+create DATABASE econdb;
+USE econdb;
