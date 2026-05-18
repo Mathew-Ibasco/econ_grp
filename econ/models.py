e36f63aa137f018273b97e68bd2afe76fb128eb6
Bea <annabianca.mendoza@uap.asia>
2026-05-18 19:33:40 +0800

    Reapply "login and database (please read models.py)"
    This reverts commit 2c991ecc16060a367473fb4c3fd68ada614f8442.

--- econ/models.py
+++ econ/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+# make sure you have 'econdb' database already made
+# Generate Migration files: python manage.py makemigrations
+# Apply to MySQL Database: python manage.py migrate
+
+class User(AbstractUser):
+    # AbstractUser already has: username, email, password, is_staff, is_superuser
+    # add extra fields here if needed
+    bio = models.TextField(blank=True)
+
+    class Meta:
+        db_table = "user"
+
+class vlog (models.Model):
+    vlogID = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=300)
+    filename = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = "vlog"
+
+class caption (models.Model):
+    captionID = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=300)
+    caption = models.CharField(max_length=300)
+    filename = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = "caption"
+
+class post(models.Model):
+    POST_TYPES = [
+        ("blog", "Blog"),
+        ("journal", "Journal"),
+    ]
+
+    postID = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=20, choices=POST_TYPES)
+    header = models.CharField(max_length=300)
+    text = models.TextField()
+    sources = models.TextField(blank=True)
+    tags = models.CharField(max_length=300, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "post"
+
+class post_section(models.Model):
+    sectionID = models.AutoField(primary_key=True)
+    post = models.ForeignKey(post, on_delete=models.CASCADE, related_name="sections")
+    subheader = models.CharField(max_length=300, blank=True)
+    text = models.TextField()
+    order = models.PositiveIntegerField()
+    # controls order
+
+    class Meta:
+        db_table = "post_section"
+        ordering = ["order"]
--- econ_web/settings.py
+++ econ_web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -138,3 +143,5 @@ STATIC_ROOT = BASE_DIR / "static_collected/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/static/'
+
+AUTH_USER_MODEL = "econ.User"
